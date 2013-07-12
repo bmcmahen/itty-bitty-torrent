35fc6f5629818891661a8a0f93e16b2265703c2c
Ben McMahen <ben.mcmahen@gmail.com>
2013-07-12 15:18:19 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,180 @@
+var Swarm = require('peer-wire-swarm');
+var readTorrent = require('read-torrent');
+var hat = require('hat');
+var speedometer = require('speedometer');
+var bitfield = require('bitfield');
+
+var Storage = require('./storage');
+
+var remove = function(arr, item) {
+	if (!arr) return false;
+	var i = arr.indexOf(item);
+	if (i === -1) return false;
+	arr.splice(i, 1);
+	return true;
+};
+
+// Constants
+var BLOCK_SIZE = 16*1024;
+var MAX_PEERS = 30;
+var MIN_PEERS = 0;
+var MAX_QUEUED = 5;
+var MIN_SPEED = 5 * 1024;
+var CHOKE_TIMEOUT = 5000;
+var PIECE_TIMEOUT = 30000;
+
+function Torrent(file, path, fn){
+	this.file = file;
+	this.peerId = '-TD0005-'+hat(48);
+	this.requesting = [];
+	this.speed = speedometer();
+	var self = this;
+	this.readTorrent(function(err, torrent){
+		if (err && fn) return fn(err);
+		self.storage = new Storage(torrent, {
+			path : path
+		});
+		self.storage.on('finished', function(){ console.log('finished')});
+		self.storage.on('readable', self.onStorageReadable.bind(self));
+		if (fn) fn();
+	});
+};
+
+module.exports = Torrent;
+
+Torrent.prototype.onStorageReadable = function(i){
+	delete this.requesting[i];
+	this.have.set(i);
+	this.swarm.wires.forEach(function(wire){
+		wire.have(i);
+	});
+};
+
+Torrent.prototype.readTorrent = function(fn){
+	var self = this;
+	readTorrent(this.file, function(err, torrent){
+		if (err) return fn(err);
+		self.torrent = torrent;
+		self.have = bitfield(torrent.pieces.length);
+		return fn(null, torrent);
+	});
+};
+
+// All connections start off as 'not interested' and 'choked'
+// In order to get to the state where you can receive files, you
+// need to send your peer an 'Interested' message, and they need
+// to send you an Unchoke message. You should wait for this unchoke message
+// from your peer before requesting pieces. Once you are unchoked
+// a client can still sned you a Choke message at any time, at which point you
+// should refrain from requestin pieces from that peer.
+
+Torrent.prototype.swarm = function(){
+	var swarm = this.swarm = Swarm(this.torrent.infoHash, this.peerId);
+	var self = this;
+
+	swarm.on('wire', function(wire){
+
+		var onchoketimeout = function(){
+			var l = swarm.wires.length;
+			if (l > MIN_PEERS && swarm.queued > 2 * MAX_PEERS - l){
+				return wire.emit('close');
+			}
+			wire.timeout = setTimeout(onchoketimeout, 5000);
+		};
+
+		wire.speed = speedometer();
+		wire.on('unchoke', self.update.bind(self));
+		wire.on('unchoke', function(){
+			if (wire.timeout) clearTimeout(wire.timeout);
+		});
+		wire.on('choke', function(){
+			if (wire.timeout) clearTimeout(wire.timeout);
+			wire.timeout = setTimeout(onchoketimeout, 5000);
+		});
+		wire.on('have', self.update.bind(self));
+		wire.once('interested', function(){ wire.unchoke(); });
+		wire.setTimeout(PIECE_TIMEOUT, function(){ wire.destroy(); });
+		wire.on('request', self.storage.read.bind(self.storage));
+		wire.bitfield(self.have);
+		wire.interested();
+		wire.timeout = setTimeout(onchoketimeout, 5000);
+	});
+
+	swarm.on('download', function(bytes){
+		self.speed(bytes);
+	});
+
+	swarm.listen();
+};
+
+Torrent.prototype.update = function(){
+	var self = this;
+	// This could be much more efficient. Basically, every time
+	// we get information from a wire that it has something, or it has
+	// unchoked us, we loop through _all_ of our wires and request
+	// information for each. It would make more sense to just request
+	// for each wire when it is unchoked or 'have', and then when
+	// a piece is finished, we make another request from that peer.
+	this.swarm.wires.forEach(function(peer){
+		if (peer.peerChoking) return;
+		self.select(peer);
+		if (!peer.requests && self.storage.missing.length < 30)
+			self.select(peer, true);
+	});
+};
+
+// Request the pieces that we are missing. Missing pieces
+// are stored in Storage.
+Torrent.prototype.select = function(peer, force){
+	var storage = this.storage;
+	var requesting = this.requesting;
+	var peerOffset = this.calculateOffset(peer);
+
+	storage.missing
+		.slice(peerOffset)
+		.some(function(piece){
+
+			if (peer.requests >= MAX_QUEUED) return true;
+
+			// Make sure that our peer actually has the piece
+			// before requesting it.
+			if (!peer.peerPieces[piece]) return;
+			var offset = storage.select(piece, force);
+			if (offset === -1) return;
+
+			requesting[piece] = requesting[piece] || [];
+			requesting[piece].push(peer);
+
+			peer.request(piece, offset, storage.sizeof(piece, offset), function(err, buffer){
+				remove(requesting[piece], peer);
+				if (err) return storage.deselect(piece, offset);
+				storage.write(piece, offset, buffer);
+			});
+		});
+};
+
+Torrent.prototype.calculateOffset = function(me){
+	var speed = me.speed();
+	var time = MAX_QUEUED * BLOCK_SIZE / (speed || 1);
+	var max = this.storage.missing.length > 60
+		? this.storage.missing.length - 30
+		: this.storage.missing.length - 1;
+	var data = 0;
+	var self = this;
+
+	this.swarm.wires.forEach(function(wire){
+		if (!wire.peerPieces[self.storage.missing[0]]) return;
+		if (wire.peerChoking) return;
+		if (me === wire || wire.speed() < speed) return;
+		data += wire.speed() * time;
+	});
+
+	return Math.min(Math.floor(data / this.torrent.pieceLength), max);
+};
+
+
+var path = __dirname + '/testdownload/';
+var src = __dirname + '/torrent.torrent';
+var tor = new Torrent(src, path, function(err){
+	if (!err) tor.swarm();
+});
---
+++ storage.js
@@ -0,0 +1,172 @@
+// storage
+
+var partFile = require('part-file');
+var piece = require('./piece');
+var fs = require('fs');
+var events = require('events');
+var util  = require('util');
+var nodePath = require('path');
+
+/**
+ * Constructor: Each file contained within the Torrent
+ * @param {Object} file
+ * @param {Object} torrent
+ * @param {String} path
+ */
+
+function File(file, torrent, path){
+	if (!(this instanceof File)) return new File(file, torrent, path);
+	events.EventEmitter.call(this);
+	this.torrent = torrent;
+	this.path = nodePath.join(path, file.name);
+	this.name = file.name;
+	this.length = file.length;
+	this.offset = file.offset;
+	this.getPosition().tryToResume();
+};
+
+util.inherits(File, events.EventEmitter);
+
+/**
+ * Determine our Position within Torrent PieceLength
+ * and create our destination file.
+ * @return {File}
+ */
+
+File.prototype.getPosition = function(){
+	this.start = this.offset / this.torrent.pieceLength | 0;
+	this.end = ((this.offset + this.length + 1) / this.torrent.pieceLength) | 0;
+	var sliced = this.torrent.pieces.slice(this.start, this.end + 1);
+	this.destination = partFile(this.path, this.torrent.pieceLength, sliced);
+	this.destination.on('readable', this.onReadable.bind(this));
+	return this;
+};
+
+/**
+ * Let our Storage know when a part becomes readable
+ * so that it can be shared with peers.
+ * @param  {Number} i
+ */
+
+File.prototype.onReadable = function(i){
+	i += this.start;
+	this.emit('partReadable', i);
+};
+
+/**
+ * Check to see if we should resume our download.
+ */
+
+File.prototype.tryToResume = function(){
+	var self = this;
+	fs.exists(this.path, function(exists){
+		if (!exists) return;
+		var i = self.start;
+		var verifyNext = function(){
+			self.destination.verify(i, function(){
+				if (++i <= self.end) verifyNext();
+			});
+		};
+		verifyNext();
+	});
+};
+
+
+/**
+ * Constructor - Wrapper for Files & keeps track of
+ * our pieces and missing pieces.
+ * @param {Object} torrent
+ * @param {Object} options
+ */
+
+function Storage(torrent, options){
+	if (!(this instanceof Storage)) return new Storage(torrent, options);
+	events.EventEmitter.call(this);
+	this.torrent = torrent;
+	this.options = options || {};
+	this.path = options.path;
+	this.missing = [];
+
+	var self = this;
+	this.files = this.torrent.files.map(function(file){
+		var f = new File(file, torrent, options.path, self);
+		f.on('partReadable', self.onPartReadable.bind(self));
+		return f;
+	});
+
+	this.getPieces();
+};
+
+module.exports = Storage;
+
+util.inherits(Storage, events.EventEmitter);
+
+Storage.prototype.getPieces = function(){
+	var self = this;
+	var torrent = this.torrent;
+	var lastFile = torrent.files[torrent.files.length - 1];
+
+	this.pieces = torrent.pieces.map(function(_, i){
+		self.missing.push(i);
+		if (i === torrent.pieces.length - 1)
+			return piece(((lastFile.length + lastFile.offset) % torrent.pieceLength || torrent.pieceLength));
+		return piece(torrent.pieceLength);
+	});
+};
+
+Storage.prototype.onPartReadable = function(index){
+	var i = this.missing.indexOf(index);
+	this.pieces[index] = null;
+	if (i > -1) this.missing.splice(i, 1);
+	this.emit('readable', index);
+	if (this.pieces.every(function(piece){ return !piece }))
+		this.emit('finished');
+};
+
+Storage.prototype.sizeof = function(index, offset){
+	var p = this.pieces[index];
+	return p ? p.sizeof(offset) : 0;
+};
+
+Storage.prototype.select = function(index, force){
+	var p = this.pieces[index];
+	if (!p) return -1;
+	var i = p.select();
+	return i === -1 && force ? p.select(true) : i;
+};
+
+Storage.prototype.deselect = function(index, offset){
+	var p = this.pieces[index];
+	if (p) p.deselect(offset);
+};
+
+Storage.prototype.findDestination = function(i){
+	var destinationFile;
+	this.files.some(function(file){
+		if (i >= file.start && i <= file.end) {
+			destinationFile = file;
+			return true;
+		}
+	});
+	return destinationFile;
+}
+
+Storage.prototype.write = function(index, offset, block){
+	var p = this.pieces[index];
+	if (!p) return;
+	var buffer = p.write(offset, block);
+	if (!buffer) return;
+	var file = this.findDestination(index);
+	file.destination.write(index - file.start, buffer, function(err){
+		if (err) return p.reset();
+	});
+};
+
+Storage.prototype.read = function(i, offset, l, fn){
+	var file = this.findDestination(i);
+	file.destination.read(i - file.start, function(err, buffer){
+		if (err) return fn(err);
+		fn(null, buffer.slice(offset, offset + l));
+	});
+};
+
